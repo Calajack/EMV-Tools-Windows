414eb5598c0ed76197e31ddc512dc5542c6e50f9
Calajack <calajack25@gmail.com>
2025-04-01 20:10:51 +0100

    Update emv_commands.h

--- EMV-Tools-Windows/Library's/libcore/emv_commands.h
+++ EMV-Tools-Windows/Library's/libcore/emv_commands.h
@@ -1,33 +1,90 @@
+// emv_commands.h - High-level EMV command wrappers
 #ifndef EMV_COMMANDS_H
 #define EMV_COMMANDS_H
 
+#include "scard_common.h"
 #include "tlv.h"
-#include <stdint.h>
-#include <winscard.h>
+#include <stdbool.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-// APDU response structure
-typedef struct {
-    uint8_t* data;
-    size_t len;
-    uint16_t sw;
-} emv_response_t;
+/**
+ * SELECT application by AID
+ * @param scard Smart card context
+ * @param aid Application ID
+ * @param aid_len Length of AID
+ * @return TLV database with response or NULL on error
+ */
+struct tlvdb *emv_select(struct sc *scard, const unsigned char *aid, size_t aid_len);
 
-// Command builders
-emv_response_t* emv_get_challenge(SCARDHANDLE hCard);
-tlvdb_t* emv_select(SCARDHANDLE hCard, const tlv_t* aid_tlv);
-emv_response_t* emv_read_record(SCARDHANDLE hCard, uint8_t sfi, uint8_t record);
-tlvdb_t* emv_gpo(SCARDHANDLE hCard, const tlv_t* pdol_data_tlv);
+/**
+ * GET PROCESSING OPTIONS
+ * @param scard Smart card context
+ * @param pdol_data_tlv PDOL data TLV (can be NULL)
+ * @return TLV database with response or NULL on error
+ */
+struct tlvdb *emv_get_processing_options(struct sc *scard, const struct tlvdb *pdol_data_tlv);
 
-// Transaction commands
-tlvdb_t* emv_generate_ac(SCARDHANDLE hCard, uint8_t type, const tlv_t* crm_tlv);
-tlvdb_t* emv_internal_authenticate(SCARDHANDLE hCard, const tlv_t* data_tlv);
+/**
+ * READ RECORD
+ * @param scard Smart card context
+ * @param sfi Short File Identifier
+ * @param record_no Record number
+ * @return TLV database with response or NULL on error
+ */
+struct tlvdb *emv_read_record(struct sc *scard, unsigned char sfi, unsigned char record_no);
 
-// Memory management
-void emv_free_response(emv_response_t* resp);
+/**
+ * GET DATA
+ * @param scard Smart card context
+ * @param tag Tag to request
+ * @return TLV database with response or NULL on error
+ */
+struct tlvdb *emv_get_data(struct sc *scard, tlv_tag_t tag);
+
+/**
+ * INTERNAL AUTHENTICATE
+ * @param scard Smart card context
+ * @param data_tlv Data for authentication
+ * @return TLV database with response or NULL on error
+ */
+struct tlvdb *emv_internal_authenticate(struct sc *scard, const struct tlvdb *data_tlv);
+
+/**
+ * GENERATE AC (Application Cryptogram)
+ * @param scard Smart card context
+ * @param ref_control Reference control
+ * @param cdol_data_tlv CDOL data TLV
+ * @return TLV database with response or NULL on error
+ */
+struct tlvdb *emv_generate_ac(struct sc *scard, unsigned char ref_control, const struct tlvdb *cdol_data_tlv);
+
+/**
+ * GET CHALLENGE
+ * @param scard Smart card context
+ * @return TLV database with response or NULL on error
+ */
+struct tlvdb *emv_get_challenge(struct sc *scard);
+
+/**
+ * VERIFY PIN
+ * @param scard Smart card context
+ * @param pin PIN data (encoded according to EMV rules)
+ * @param pin_len Length of PIN data
+ * @return TLV database with response or NULL on error
+ */
+struct tlvdb *emv_verify_pin(struct sc *scard, const unsigned char *pin, size_t pin_len);
+
+/**
+ * Read all records from application
+ * @param scard Smart card context
+ * @param psn PSN value from processing options
+ * @param pinfo Processing info from FCI
+ * @return TLV database with all records or NULL on error
+ */
+struct tlvdb *emv_read_records(struct sc *scard, unsigned char psn, const struct tlvdb *pinfo);
 
 #ifdef __cplusplus
 }

1a0fa0e8b9b74e5782d0fd25333a8ab873658470
Calajack <calajack25@gmail.com>
2025-04-01 18:35:45 +0100

    Update tlv.h

--- EMV-Tools-Windows/Library's/libcore/tlv.h
+++ EMV-Tools-Windows/Library's/libcore/tlv.h
@@ -1,41 +1,80 @@
-#ifndef TLV_H
-#define TLV_H
+// tlv.h - Windows compatible version
+#ifndef EMV_TLV_H
+#define EMV_TLV_H
 
-#include <stdint.h>
 #include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef uint16_t tlv_tag_t;
+/* TLV tag constants from EMV specification */
+#define TLV_TAG_CLASS_MASK       0xc0
+#define TLV_TAG_COMPLEX          0x20
+#define TLV_TAG_PRIMITIVE        0x00
+#define TLV_TAG_APPLICATION      0x40
+#define TLV_TAG_CONTEXT_SPECIFIC 0x80
 
-// TLV structure (cache-aligned for Windows)
-typedef struct __declspec(align(8)) {
+/* TLV type */
+typedef uint32_t tlv_tag_t;
+
+/* TLV structure */
+struct tlv {
     tlv_tag_t tag;
     size_t len;
-    uint8_t* value;
-} tlv_t;
+    unsigned char *value;
+};
 
-// TLV Database node
-typedef struct tlvdb {
-    tlv_t tlv;
-    struct tlvdb* next;
-    struct tlvdb* child;
-} tlvdb_t;
+/* TLV database structure */
+struct tlvdb {
+    struct tlv tag;
+    struct tlvdb *next;
+    struct tlvdb *parent;
+    struct tlvdb *children;
+};
 
-// Parser functions
-tlvdb_t* tlvdb_parse(const uint8_t* buf, size_t len);
-void tlvdb_free(tlvdb_t* tlvdb);
+/* TLV database element search helpers */
+struct tlvdb_record {
+    tlv_tag_t tag;
+    const char *name;
+    const char *(*get)(const struct tlv *tlv, unsigned *clen);
+    bool (*parse)(const struct tlv *tlv);
+};
 
-// Navigation
-const tlv_t* tlvdb_get(const tlvdb_t* tlvdb, tlv_tag_t tag);
+/* Basic TLV manipulation */
+bool tlv_parse_tl(const unsigned char **buf, size_t *len, struct tlv *tlv);
+bool tlv_parse_tlv(const unsigned char **buf, size_t *len, struct tlv *tlv);
 
-// Serialization
-uint8_t* tlv_encode(const tlv_t* tlv, size_t* out_len);
+/* TLV database functions */
+struct tlvdb *tlvdb_fixed(tlv_tag_t tag, size_t len, const unsigned char *value);
+struct tlvdb *tlvdb_parse(const unsigned char *buf, size_t len);
+struct tlvdb *tlvdb_parse_multi(const unsigned char *buf, size_t len);
+struct tlvdb *tlvdb_decode(const struct tlvdb *tlvdb, tlv_tag_t tag, size_t *len, unsigned char **buf);
+
+void tlvdb_free(struct tlvdb *tlvdb);
+void tlvdb_add(struct tlvdb *tlvdb, struct tlvdb *other);
+void tlvdb_visit(const struct tlvdb *tlvdb, void (*func)(const struct tlv *tlv, void *data), void *data);
+
+const struct tlv *tlvdb_get(const struct tlvdb *tlvdb, tlv_tag_t tag, const struct tlv *prev);
+const struct tlvdb *tlvdb_get_tlvdb(const struct tlvdb *tlvdb, tlv_tag_t tag);
+const struct tlvdb *tlvdb_find(const struct tlvdb *tlvdb, tlv_tag_t tag);
+const struct tlvdb *tlvdb_find_next(const struct tlvdb *tlvdb, const struct tlvdb *prev);
+const struct tlv *tlvdb_get_inchild(const struct tlvdb *tlvdb, tlv_tag_t tag, const struct tlv *prev);
+unsigned char *tlv_encode(const struct tlv *tlv, size_t *len);
+struct tlvdb *tlvdb_elm_get_next(struct tlvdb *tlvdb);
+struct tlvdb *tlvdb_elm_get_children(struct tlvdb *tlvdb);
+
+/* Utility functions */
+bool tlv_is_constructed(const struct tlv *tlv);
+const char *tlv_tag_get_name(tlv_tag_t tag);
+const char *tlv_tag_get_description(tlv_tag_t tag);
+void tlv_tag_dump(const struct tlv *tlv, FILE *f, int level);
+void tlvdb_dump(const struct tlvdb *tlvdb, FILE *f);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif // TLV_H
+#endif

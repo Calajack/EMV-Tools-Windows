2dfc0eecca3e0284771e5fef4947b017df59a4a6
Calajack <calajack25@gmail.com>
2025-04-03 15:37:23 +0100

    Update emv_tags.h

--- EMV-Tools-Windows/Library's/libcore/emv_tags.h
+++ EMV-Tools-Windows/Library's/libcore/emv_tags.h
@@ -20,7 +20,7 @@ extern "C" {
 #define EMV_TAG_AID            0x4F     // Application Identifier
 #define EMV_TAG_PDOL           0x9F38   // Processing Options Data Object List
 #define EMV_TAG_CDOL1          0x8C     // Card Risk Management Data Object List 1
-#define EMV_TAG_DDOL           0x9F49   // Dynamic Data Authentication Data Object List
+#define EMV_TAG_DDOL           0x9F49   // Dynamic Data Authentication Data Object List 
 
 // Tag information structure
 typedef struct {
@@ -29,15 +29,15 @@ typedef struct {
     const char* description;
     size_t min_len;
     size_t max_len;
-} emv_tag_def_t;
+};
 
-typedef struct {
+typedef struct emv_tag_def_t {
     uint16_t tag;
     const char* name;
     const char* description;
     emv_tag_format_t format; // Using the enum you defined in emv_defs.h
     const uint8_t* bitmask;
-} emv_tag_def_t;
+}  emv_tag_def_t;
 
 const emv_tag_info_t* emv_tag_get_info(uint16_t tag);
 int emv_tag_is_constructed(uint16_t tag);

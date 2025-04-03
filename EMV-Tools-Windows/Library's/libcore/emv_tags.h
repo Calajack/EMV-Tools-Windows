2586e006e005d6c56bd94aa6002b74e27ed892e4
Calajack <calajack25@gmail.com>
2025-04-03 21:33:54 +0100

    Update emv_tags.h

--- EMV-Tools-Windows/Library's/libcore/emv_tags.h
+++ EMV-Tools-Windows/Library's/libcore/emv_tags.h
@@ -4,19 +4,18 @@
 #include <stdint.h>
 #include "emv_defs.h"
 
-   typedef struct emv_tag_info_t {
-       uint16_t tag;
-       const char* name;
-       const char* type;
-       const char* description;
-   } emv_tag_info_t;
-
+typedef struct emv_tag_info_t {
+    uint16_t tag;
+    const char* name;
+    const char* type;
+    const char* description;
+} emv_tag_info_t;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-// Publicly exposed tags (used in APIs)
+    // Publicly exposed tags (used in APIs)
 #define EMV_TAG_PAN            0x5A     // Primary Account Number
 #define EMV_TAG_EXPIRY         0x5F24   // Expiration Date
 #define EMV_TAG_AID            0x4F     // Application Identifier
@@ -24,29 +23,32 @@ extern "C" {
 #define EMV_TAG_CDOL1          0x8C     // Card Risk Management Data Object List 1
 #define EMV_TAG_DDOL           0x9F49   // Dynamic Data Authentication Data Object List 
 
-// Tag information structure
+// Tag information structure - this appears to be an incomplete struct
+// If not needed, you can remove it
     typedef struct {
         uint16_t tag;
         const char* name;
         const char* description;
         size_t min_len;
         size_t max_len;
-    };
+    } emv_tag_length_info_t;  // Added a name to make it complete
 
-    typedef struct emv_tag_def_t {
+    typedef struct emv_tag_def {
         uint16_t tag;
         const char* name;
         const char* type;
         const char* description;
-        emv_tag_format_t* format; // Using the enum you defined in emv_defs.h
+        emv_tag_format_t format;  // Changed from pointer to direct type
         const uint8_t* bitmask;
     } emv_tag_def_t;
 
-    const emv_tag_info_t; *emv_tag_get_info(uint16_t tag);
-int emv_tag_is_constructed(uint16_t tag);
-extern const char* tvr_bits[5][8];
+    // Function declarations
+    const emv_tag_info_t* emv_tag_get_info(uint16_t tag);
+    int emv_tag_is_constructed(uint16_t tag);
 
-extern const struct emv_tag_def tag_database[];
+    // External declarations
+    extern const char* tvr_bits[5][8];
+    extern const struct emv_tag_def tag_database[];
 
 #ifdef __cplusplus
 }

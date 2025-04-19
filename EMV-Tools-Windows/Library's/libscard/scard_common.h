1a2073ab1675d422848e2657b7a5d497a3283825
Calajack <calajack25@gmail.com>
2025-04-19 18:22:41 +0100

    Update scard_common.h

--- EMV-Tools-Windows/Library's/libscard/scard_common.h
+++ EMV-Tools-Windows/Library's/libscard/scard_common.h
@@ -17,7 +17,7 @@ extern "C" {
 #define OMNIKEY_LED_OFF      0x00
 #define OMNIKEY_LED_GREEN    0x01
 
-    // Error codes (Windows-aligned)
+// Error codes (Windows-aligned)
 #define SCARD_SUCCESS 0
 #define SCARD_ERR_INVALID_PARAM 0x80100004
 #define SCARD_ERR_NO_SERVICE 0x8010001D
@@ -26,86 +26,75 @@ extern "C" {
 #define APDU_MAX_LEN 256
 #define SW1SW2_OK 0x9000
 
+// Interface types
+typedef enum {
+    SCARD_IFD_AUTO = 0,
+    SCARD_IFD_ICC,
+    SCARD_IFD_PICC,
+    SCARD_IFD_MANUAL
+} SCardInterfaceType;
+
 // Smart card operation modes
-    typedef enum {
-        SCARD_MODE_NORMAL = 0,
-        SCARD_MODE_EMULATE,
-        SCARD_MODE_MANUAL
-    } SCardMode;
+typedef enum {
+    SCARD_MODE_NORMAL = 0,
+    SCARD_MODE_EMULATE,
+    SCARD_MODE_MANUAL
+} SCardMode;
 
-    // Interface types
-    typedef enum {
-        SCARD_IFD_AUTO = 0,
-        SCARD_IFD_ICC,
-        SCARD_IFD_PICC,
-        SCARD_IFD_MANUAL
-    } SCardInterfaceType;
+// Emulator context
+typedef struct {
+    uint8_t (*emulate_cb)(const uint8_t *apdu, size_t apdu_len, 
+                         uint8_t *resp, size_t *resp_len);
+} SCardEmuContext;
 
-    // Emulator context
-    typedef struct {
-        uint8_t(*emulate_cb)(const uint8_t* apdu, size_t apdu_len,
-            uint8_t* resp, size_t* resp_len);
-    } SCardEmuContext;
+// Manual context
+typedef struct {
+    uint8_t atr[32];
+    size_t atr_len;
+    uint8_t responses[16][256];
+    size_t resp_lens[16];
+    size_t resp_count;
+    int dummy;
+} SCardManualContext;
 
-    // Manual context
-    typedef struct {
-        uint8_t atr[32];
-        size_t atr_len;
-        uint8_t responses[16][256];
-        size_t resp_lens[16];
-        size_t resp_count;
-        int dummy;
-    } SCardManualContext;
+// Reader state
+typedef struct {
+    char reader_name[256];
+    DWORD state;
+    DWORD protocol;
+} SCardReaderState;
 
-    // Reader state
-    typedef struct {
-        char reader_name[256];
-        DWORD state;
-        DWORD protocol;
-    } SCardReaderState;
+// Context structure
+typedef struct {
+    SCARDCONTEXT hContext;
+    SCARDHANDLE hCard;
+    DWORD dwProtocol;
+    uint8_t atr[64];
+    size_t atr_len;
+} SCardContext;
 
-    // Context structure
-    typedef struct {
-        SCARDCONTEXT hContext;
-        SCARDHANDLE hCard;
-        DWORD dwProtocol;
-        uint8_t atr[64];
-        size_t atr_len;
-    } SCardContext;
+// Bridge structure used in the application code
+typedef struct {
+    SCARDCONTEXT hContext;
+    SCARDHANDLE hCard;
+    DWORD dwActiveProtocol;
+} sc;
 
-    // Bridge structure used in the application code
-    typedef struct {
-        SCARDCONTEXT hContext;
-        SCARDHANDLE hCard;
-        DWORD dwActiveProtocol;
-    } sc;
+// Function declarations - this matches what the applications expect
+int scard_omnikey_set_led(SCARDCONTEXT* ctx, unsigned char* state);
 
-    // Function declarations for Windows PC/SC integration
-    int scard_omnikey_set_led(SCARDCONTEXT* ctx, unsigned char* state);
+// EMV tool PC/SC compatibility functions
+int scard_establish_context(sc **out);
+int scard_release_context(sc *sc_ctx);
+int scard_list_readers(sc *sc_ctx, char readers[][256], DWORD *readers_len);
+int scard_connect(sc *sc_ctx, const char *reader, DWORD share_mode, DWORD *active_protocol);
+int scard_disconnect(sc *sc_ctx);
+int scard_transmit(sc *sc_ctx, const unsigned char *send_buf, size_t send_len, 
+                  unsigned char *recv_buf, size_t *recv_len);
 
-    // Original API functions
-    int scard_establish_context(sc** out);
-    int scard_release_context(sc* sc_ctx);
-    int scard_list_readers(sc* sc_ctx, char readers[][256], DWORD* readers_len);
-    int scard_connect(sc* sc_ctx, const char* reader, DWORD share_mode, DWORD* active_protocol);
-    int scard_disconnect(sc* sc_ctx);
-    int scard_transmit(sc* sc_ctx, const unsigned char* send_buf, size_t send_len,
-        unsigned char* recv_buf, size_t* recv_len);
-
-    // Newer API functions
-    SCardContext* scard_establish(DWORD scope);
-    int scard_connect(SCardContext* ctx, LPCSTR reader, DWORD share_mode, DWORD preferred_protocols);
-    int scard_disconnect(SCardContext* ctx);
-    int scard_reconnect(SCardContext* ctx, DWORD share_mode, DWORD preferred_protocols, DWORD initialization);
-    int scard_list_readers(SCardContext* ctx, SCardReaderState* readers, size_t* count);
-    int scard_transmit(SCardContext* ctx, const uint8_t* apdu, size_t apdu_len, uint8_t* resp, size_t* resp_len);
-    int scard_begin_transaction(SCardContext* ctx);
-    int scard_end_transaction(SCardContext* ctx, DWORD disposition);
-    void scard_free(SCardContext* ctx);
-
-    // Emulator and mode functions
-    int scard_set_mode(SCardMode mode);
-    int scard_set_emu_callback(uint8_t(*cb)(const uint8_t*, size_t, uint8_t*, size_t*));
+// EMU mode functions
+int scard_set_mode(SCardMode mode);
+int scard_set_emu_callback(uint8_t(*cb)(const uint8_t*, size_t, uint8_t*, size_t*));
 
 #ifdef __cplusplus
 }

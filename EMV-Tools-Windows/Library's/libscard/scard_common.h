59fab0076c7c5776b5cd648e025533d312575a83
Calajack <calajack25@gmail.com>
2025-04-21 17:26:41 +0100

    Update scard_common.h

--- EMV-Tools-Windows/Library's/libscard/scard_common.h
+++ EMV-Tools-Windows/Library's/libscard/scard_common.h
@@ -17,7 +17,10 @@ extern "C" {
 #define OMNIKEY_LED_OFF      0x00
 #define OMNIKEY_LED_GREEN    0x01
 
-    // Error codes (Windows-aligned)
+    // Define custom attribute code not present in standard winscard.h
+#define SCARD_ATTR_VENDOR_IFD_SERIAL_TIMEOUT 0x0A0004
+
+// Error codes (Windows-aligned)
 #define SCARD_SUCCESS 0
 #define SCARD_ERR_INVALID_PARAM 0x80100004
 #define SCARD_ERR_NO_SERVICE 0x8010001D
@@ -83,26 +86,37 @@ extern "C" {
         size_t atr_len;
     } SCardContext;
 
-    // Function declarations
-    int scard_omnikey_set_led(SCardContext* ctx, unsigned char* state);
+    // Function declarations - avoid duplicate/conflicting declarations
 
-    // Add scard_establish for EMV-Tools_Win.cpp
+    // Function for EMV-Tools_Win.cpp
     SCardContext* scard_establish(DWORD scope);
 
-    // Low-level PC/SC functions using the 'sc' struct
-    bool scard_list_readers(SCARDCONTEXT hContext, char readers[][MAX_READERNAME], DWORD* reader_count, size_t max_readers, size_t max_len);
+    // Connect function for SCardContext
+    int scard_connect_ctx(SCardContext* ctx, const char* reader, DWORD share_mode);
+
+    // Omnikey LED control
+    int scard_omnikey_set_led(SCardContext* ctx, unsigned char state);
+
+    // Simplified PC/SC functions used by tool programs
     LONG scard_establish_context(SCARDCONTEXT* ctx);
     void scard_release_context(SCARDCONTEXT ctx);
+    const char* pcsc_stringify_error(LONG code);
     LONG scard_connect(SCARDCONTEXT ctx, const char* reader, SCARDHANDLE* card, DWORD* protocol);
     void scard_disconnect(SCARDHANDLE card, DWORD disposition);
-    const char* pcsc_stringify_error(LONG code);
+    bool scard_list_readers(SCARDCONTEXT hContext, char readers[][MAX_READERNAME], DWORD* reader_count, size_t max_readers, size_t max_len);
 
-    // Update connect signature to match what EMV-Tools_Win.cpp expects
-    int scard_connect(SCardContext* ctx, const char* reader, DWORD share_mode);
+    // SCardContext functions
+    int scard_disconnect_ctx(SCardContext* ctx);
+    int scard_transmit(SCardContext* ctx, const uint8_t* apdu, size_t apdu_len, uint8_t* resp, size_t* resp_len);
+    int scard_transmit_ex(SCardContext* ctx, const uint8_t* apdu, size_t apdu_len, uint8_t* resp, size_t* resp_len, DWORD timeout_ms);
+    void scard_free(SCardContext* ctx);
 
     // EMU mode functions
     int scard_set_mode(SCardMode mode);
     int scard_set_emu_callback(uint8_t(*cb)(const uint8_t*, size_t, uint8_t*, size_t*));
+
+    // Custom timeout function
+    int SCardSetTimeout(SCARDHANDLE hCard, DWORD dwTimeout);
 
 #ifdef __cplusplus
 }
